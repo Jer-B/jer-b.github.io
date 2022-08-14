92a1d3b303abe56b7dd13bfb42107ffbb8f5645e
Jer-B <111217617+Jer-B@users.noreply.github.com>
2022-08-14 13:39:34 +0200

    Site Upload View Test

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+
+// // Nav selections
+// const burger = document.querySelector("#burger-menu");
+// const ul = document.querySelector("nav ul");
+// const nav = document.querySelector("nav");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// // Select nav
+// const navLink = document.querySelectorAll(".nav-link");
+
+// //　menu function
+// burger.addEventListener("click", () => {
+//   ul.classList.toggle("show");
+// });
+
+// // Close　menu when clicked
+// navLink.forEach((link) =>
+//   link.addEventListener("click", () => {
+//     ul.classList.remove("show");
+//   })
+// );
+
+// scroll
+// scroll to top functionality
+// const scrollUp = document.querySelector("#scroll-up");
+
+// scrollUp.addEventListener("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// });
