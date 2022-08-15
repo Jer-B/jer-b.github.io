d0991496648ef72a4c8e719a7198356c09472d25
Jer-B <111217617+Jer-B@users.noreply.github.com>
2022-08-15 11:34:38 +0200

    Various Design Fixes and Added content
    solidity page and Art page added

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,15 @@
 // const ul = document.querySelector("nav ul");
 // const nav = document.querySelector("nav");
 
-// Scroll to top selection
-const scrollUp = document.querySelector("#scroll-up");
+// // Scroll to top selection
+// const scrollUp = document.querySelector("#scroll-up");
+// // scroll to top functionality
+// scrollUp.addEventListener("click", () => {
+//     window.scrollTo({
+//       top: 0,
+//       left: 0,
+//       behavior: "smooth",
+//     });
 
 // // Select nav
 // const navLink = document.querySelectorAll(".nav-link");
